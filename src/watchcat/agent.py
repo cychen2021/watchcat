0d0b692c966b5189974ddcb3db2da9713da46160
Chuyang Chen <chuyangchen2018@outlook.com>
2025-05-10 17:22:47 -0400

    Update agent module and dependencies
    - Refactor agent module for improved structure
    - Update dependencies in `uv.lock`

--- src/watchcat/agent.py
+++ src/watchcat/agent.py
@@ -5,6 +5,19 @@ from phdkit.configlib import configurable, setting, ConfigReader
 
 def __read_config(config_file: str | None = None):
     with open(config_file if config_file is not None else "config.toml", "rb") as f:
+        config = tomllib.load(f)
+    def check_keys(d: dict):
+        for k, v in d.items():
+            assert isinstance(k, str), f"Key {k} is not a string"
+            if k.endswith("api_key"):
+                raise ValueError(f"You should put API keys in the env.toml file")
+            if isinstance(v, dict):
+                check_keys(v)
+    check_keys(config)
+    return config
+
+def __read_env_config(config_file: str | None = None):
+    with open(config_file if config_file is not None else "env.toml", "rb") as f:
         return tomllib.load(f)
 
 class ProviderInfo(ABC):
@@ -14,16 +27,40 @@ class ProviderInfo(ABC):
 class GeminiProviderInfo(ProviderInfo):
     api_key: str
 
-@configurable(__read_config)
+@dataclass
+class ModelInfo:
+    generation_model: str | None
+    embedding_model: str | None
+
+@configurable(__read_config, __read_env_config)
 class Agent:
     @property
     def model(self):
-        return self.__model
+        if self.__model is None:
+            return None
+        return self.__model.generation_model
 
     @model.setter
     @setting("model")
     def model(self, model):
-        self.__model = model
+        if self.__model is None:
+            self.__model = ModelInfo(generation_model=model, embedding_model=None)
+        else:
+            self.__model.generation_model = model
+
+    @property
+    def embedding_model(self):
+        if self.__model is None:
+            return None
+        return self.__model.embedding_model
+
+    @embedding_model.setter
+    @setting("embedding_model")
+    def embedding_model(self, embedding_model):
+        if self.__model is None:
+            self.__model = ModelInfo(generation_model=None, embedding_model=embedding_model)
+        else:
+            self.__model.embedding_model = embedding_model
 
     @property
     def gemini_api_key(self):
