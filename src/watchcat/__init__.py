8ba202c4d4c79e6bcb6e61c6a4e63d696b047eda
Chuyang Chen <chuyangchen2018@outlook.com>
2025-05-20 20:12:11 -0400

    Refactor main function in `__init__.py`
    - Change `--debug` option to `--develop`
    - Update function parameter from `debug` to `develop`

--- src/watchcat/__init__.py
+++ src/watchcat/__init__.py
@@ -5,9 +5,9 @@ from .agent import Agent, logger as agent_logger
 
 
 @click.command()
-@click.option("--debug", is_flag=True, help="Enable debug mode.", default=True)
-def main(debug: bool) -> None:
-    if not debug:
+@click.option("--develop", is_flag=True, help="Enable in-development mode.", default=True)
+def main(develop: bool) -> None:
+    if not develop:
         email_notifier = EmailNotifier()
         config[email_notifier].load("watchcat.config.toml", "watchcat.env.toml")
         agent_logger.add_output(LogOutput.email(email_notifier))
