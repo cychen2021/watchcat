1bdbd6bafb813dfa750383fdd1cb28c0c89d6c76
Chuyang Chen <chuyangchen2018@outlook.com>
2025-05-21 03:16:44 -0400

    Enhance logging configuration in `__init__.py`
    - Add `LogLevel` to `LogOutput.email` for warning level
    - Implement timestamped log file paths for Windows and Unix
    - Ensure `stderr` output is always added

--- src/watchcat/__init__.py
+++ src/watchcat/__init__.py
@@ -1,16 +1,28 @@
-from phdkit.log import LogOutput, EmailNotifier
+from phdkit.log import LogOutput, EmailNotifier, LogLevel
 from phdkit.configlib import config
 import click
+import platform
+import os
+from datetime import datetime, UTC
 from .agent import Agent, logger as agent_logger
 
 
 @click.command()
 @click.option("--develop", is_flag=True, help="Enable in-development mode.", default=True)
 def main(develop: bool) -> None:
+    timestamp = datetime.now(UTC).strftime("%Y-%m-%d_%H:%M:%S")
     if not develop:
         email_notifier = EmailNotifier()
         config[email_notifier].load("watchcat.config.toml", "watchcat.env.toml")
-        agent_logger.add_output(LogOutput.email(email_notifier))
+        agent_logger.add_output(LogOutput.email(email_notifier, level=LogLevel.WARNING))
+    agent_logger.add_output(LogOutput.stderr())
+    if platform.system() == "Windows":
+        appdata = os.getenv('LOCALAPPDATA')
+        assert appdata is not None, "LOCALAPPDATA is not set"
+        log_path = os.path.realpath(os.path.join(appdata, f"watchcat_{timestamp}.jsonl"))
+    else:
+        log_path = os.path.realpath(os.path.join("/var/log", f"watchcat_{timestamp}.jsonl"))
+    agent_logger.add_output(LogOutput.file(log_path, level=LogLevel.DEBUG, format="jsonl"))
 
     agent = Agent()
     config[agent].load("watchcat.config.toml", "watchcat.env.toml")
