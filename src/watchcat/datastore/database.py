221eea6e064e09cf8c29eba9ac857e95022e3098
Chuyang Chen <chuyangchen2018@outlook.com>
2025-06-27 12:28:00 -0400

    Add configuration functions to `Database` and `SQLite3DB`
    - Implement `__read_config` and `__read_env` functions
    - Update `Database` class with `@setting` for `db_path`
    - Adjust `SQLite3DB` to include `init_script` getter and setter

--- src/watchcat/datastore/database.py
+++ src/watchcat/datastore/database.py
@@ -1,9 +1,21 @@
+from phdkit.util import todo
+from phdkit.configlib import configurable, setting
 from abc import ABC, abstractmethod
 from phdkit.util import unimplemented
 
+def __read_config(config_file: str | None = None):
+    return todo()
+
+def __read_env(config_file: str | None = None):
+    return todo()
+
+@configurable(__read_config, config_key="datastore", load_env=__read_env)
 class Database(ABC):
+    @setting("path")
+    def db_path(self) -> str: ...
+
     def __init__(self, init_script: str):
-        self.init_script = init_script
+        self._init_script = init_script
 
     @abstractmethod
     def open(self):
--- src/watchcat/datastore/sqlite3_db.py
+++ src/watchcat/datastore/sqlite3_db.py
@@ -1,7 +1,24 @@
 import sqlite3
+from  phdkit.configlib import configurable, setting
+from phdkit.util import todo
 from .database import Database
 
+def __read_config(config_file: str | None = None):
+    return todo()
+
+def __read_env(config_file: str | None = None):
+    return todo()
+
+@configurable(__read_config, config_key="store.sqlite3", load_env=__read_env)
 class SQLite3DB(Database):
+    @setting.getter("init_script")
+    def init_script(self) -> str:
+        return self._init_script
+
+    @init_script.setter
+    def _set_init_script(self, value: str):
+        self._init_script = value
+
     def __init__(self, init_script: str):
         super().__init__(init_script)
         self.connection: sqlite3.Connection
