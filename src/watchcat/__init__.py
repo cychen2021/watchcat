4670083a667cca0b49c6ba16121fb6565707be6e
Chuyang Chen <chuyangchen2018@outlook.com>
2025-08-19 22:32:03 -0400

    Add archive email option to main command
    - Introduced `--archive-email/--no-archive-email` option to the `main` function
    - Updated function signature to include `archive_email` parameter

--- src/watchcat/__init__.py
+++ src/watchcat/__init__.py
@@ -4,5 +4,6 @@ from phdkit import unimplemented
 
 @click.command()
 @click.option("--develop/--prod", help="Enable in-development mode.", default=True)
-def main(develop: bool) -> None:
+@click.option("--archive-email/--no-archive-email", help="Archive processed emails.", default=False, show_default=True)
+def main(develop: bool, archive_email: bool) -> None:
     unimplemented()
