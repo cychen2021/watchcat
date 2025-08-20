e9b383d6d7e9d8d82c5e66fe127225569db74297
Chuyang Chen <chuyangchen2018@outlook.com>
2025-08-19 21:24:20 -0400

    Remove deprecated `Agent` class and related code
    - Deleted `src/watchcat/_agent.py` as it is no longer needed
    - Removed unused imports from `src/watchcat/__init__.py`

--- src/watchcat/__init__.py
+++ src/watchcat/__init__.py
@@ -1,51 +1,9 @@
-from phdkit.log import LogOutput, EmailNotifier, LogLevel
-from phdkit.configlib import config
 import click
-import platform
-import os
-import xdg_base_dirs as xdg
-from datetime import datetime, UTC
-import litellm
-from .workflow import Workflow
-
-litellm.suppress_debug_info = True
-
+from phdkit import unimplemented
 
 @click.command()
 @click.option(
     "--develop", is_flag=True, help="Enable in-development mode.", default=True
 )
 def main(develop: bool) -> None:
-    ...
-    # agent = Agent()
-    # timestamp = datetime.now(UTC).strftime("%Y-%m-%d_%H:%M:%S")
-    # if not develop:
-    #     email_notifier = EmailNotifier()
-    #     config[email_notifier].load(
-    #         "watchcat/watchcat.config.toml", "watchcat/watchcat.env.toml"
-    #     )
-    #     agent.logger.add_output(LogOutput.email(email_notifier, level=LogLevel.WARNING))
-    #     agent.logger.add_output(LogOutput.stderr())
-    # else:
-    #     agent.logger.add_output(LogOutput.stderr(level=LogLevel.DEBUG))
-    # if platform.system() == "Windows":
-    #     appdata = os.getenv("LOCALAPPDATA")
-    #     assert appdata is not None, "LOCALAPPDATA is not set"
-    #     log_path = os.path.realpath(
-    #         os.path.join(appdata, f"watchcat_{timestamp}.jsonl")
-    #     )
-    # else:
-    #     log_path = os.path.realpath(
-    #         os.path.join(xdg.xdg_state_home(), f"watchcat_{timestamp}.jsonl")
-    #     )
-    # agent.logger.add_output(
-    #     LogOutput.file(log_path, level=LogLevel.DEBUG, format="jsonl")
-    # )
-
-    # config[agent].load("watchcat/watchcat.config.toml", "watchcat/watchcat.env.toml")
-    # agent.init()
-
-    # papers = agent.fetch_papers()
-    # for paper in papers:
-    #     worth = agent.worth_reading(paper)
-    #     print(f"Paper {paper.id} worth reading: {worth}")
+    unimplemented()
