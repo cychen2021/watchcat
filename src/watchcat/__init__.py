f1bb5963436705cd05e0d1dd0ff87908cff02bb7
Chuyang Chen <chuyangchen2018@outlook.com>
2025-08-24 18:40:13 -0400

    Update configuration file paths in `__init__.py`
    - Set `DEFAULT_CONFIG_FILE` and `DEFAULT_ENV_FILE` using `xdg_config_home`
    - Modify `@configurable` decorator to use new configuration paths
    - Update `main` function to load configuration and environment files

--- src/watchcat/__init__.py
+++ src/watchcat/__init__.py
@@ -1,11 +1,15 @@
 import click
 from xdg_base_dirs import xdg_config_home
+from pathlib import Path
 from phdkit import unimplemented
 from phdkit.configlib import configurable, setting, TomlReader, config
 
+DEFAULT_CONFIG_FILE = str(Path(xdg_config_home()) / "watchcat" / "config.toml")
+DEFAULT_ENV_FILE = str(Path(xdg_config_home()) / "watchcat" / "env.toml")
+
 @configurable(
-    TomlReader("watchcat.config.toml"),
-    load_env=TomlReader("watchcat.env.toml")
+    TomlReader(DEFAULT_CONFIG_FILE),
+    load_env=TomlReader(DEFAULT_ENV_FILE)
 )
 class App:
     @setting("email.provider")
@@ -17,17 +21,30 @@ class App:
     @setting("email.password")
     def email_password(self) -> str: ...
 
+    @setting("email.archive_processed")
+    def email_archive_processed(self) -> bool: ...
+
     def run(self):
         unimplemented()
 
 @click.command()
 @click.option("--develop/--prod", help="Enable in-development mode.", default=True)
 @click.option(
-    "--archive-email/--no-archive-email",
-    help="Archive processed emails.",
-    default=False,
+    "config_file",
+    "--config",
+    "-c",
+    default=DEFAULT_CONFIG_FILE,
     show_default=True,
+    type=click.Path(exists=True)
 )
-def main(develop: bool, archive_email: bool) -> None:
+@click.option(
+    "env_file",
+    "--env",
+    "-e",
+    default=DEFAULT_ENV_FILE,
+    show_default=True,
+    type=click.Path(exists=True)
+)
+def main(develop: bool, config_file: str, env_file: str) -> None:
     app = App()
-    config[app].load()
+    config[app].load(config_file, env_file)
