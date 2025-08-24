843b00789350bcd559f3f7952a533f2752f33627
Chuyang Chen <chuyangchen2018@outlook.com>
2025-08-24 12:14:01 -0400

    Add email settings to `App` class
    - Introduced `email_provider`, `email_address`, and `email_password` settings
    - Implemented `run` method in `App` class
    - Updated `main` function to load configuration for `App`

--- src/watchcat/__init__.py
+++ src/watchcat/__init__.py
@@ -1,6 +1,23 @@
 import click
 from phdkit import unimplemented
+from phdkit.configlib import configurable, setting, TomlReader, config
 
+@configurable(
+    TomlReader("watchcat.config.toml"),
+    load_env=TomlReader("watchcat.env.toml")
+)
+class App:
+    @setting("email.provider")
+    def email_provider(self) -> str: ...
+
+    @setting("email.address")
+    def email_address(self) -> str: ...
+
+    @setting("email.password")
+    def email_password(self) -> str: ...
+
+    def run(self):
+        unimplemented()
 
 @click.command()
 @click.option("--develop/--prod", help="Enable in-development mode.", default=True)
@@ -11,4 +28,5 @@ from phdkit import unimplemented
     show_default=True,
 )
 def main(develop: bool, archive_email: bool) -> None:
-    unimplemented()
+    app = App()
+    config[app].load()
